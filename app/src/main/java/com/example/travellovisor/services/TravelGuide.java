caf00727ac6a1fe20b735ff5284804343267caf2
shovon <shovonsikder.cse.gub@gmail.com>
2021-12-29 21:16:19 +0600

    Database connection is completed!! Intent can pass data to description activity

--- app/src/main/java/com/example/travellovisor/services/TravelGuide.java
+++ app/src/main/java/com/example/travellovisor/services/TravelGuide.java
@@ -1,12 +1,12 @@
 package com.example.travellovisor.services;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -14,6 +14,11 @@ import android.widget.Toast;
 
 import com.example.travellovisor.Homepage;
 import com.example.travellovisor.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -21,32 +26,69 @@ public class TravelGuide extends AppCompatActivity {
 ImageView search2;
 TextView buttonToPkg;
 ListView guideList;
+FirebaseDatabase database;
+DatabaseReference db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_travel_guide);
-
-        //reff
+        System.out.println(db);
+        //references
         search2=findViewById(R.id.search2);
         buttonToPkg=findViewById(R.id.butonToPkg);
         guideList=findViewById(R.id.guideList);
 
-       //temporary
-        ArrayList<String> titles= new ArrayList<String>();
-        ArrayList<String> descriptions= new ArrayList<String>();
-        titles.add("TT");titles.add("TT2 from DB");
-        descriptions.add("DD");descriptions.add("DD2 freom DB");
-        GuideListAdapter adapter=new GuideListAdapter(getApplicationContext(),titles,descriptions);
+        //get database reference
+        database=FirebaseDatabase.getInstance("https://travellovisor-829f1-default-rtdb.asia-southeast1.firebasedatabase.app");
+        db=database.getReference("travelguides");
+
+       //Fetched data stored array
+        ArrayList<Guides> guides= new ArrayList<Guides>();
+
+        //Guides g1=new Guides("hi","Hello");
+        //guides.add(g1);
+
+        GuideListAdapter adapter=new GuideListAdapter(getApplicationContext(),guides);
         guideList.setAdapter(adapter);
+
 
         guideList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               //view description on another activity
                 Intent intent=new Intent(getApplicationContext(),TravelGuideDetails.class);
-
+                Bundle extras=new Bundle();
+                extras.putString("title",guides.get(position).title);
+                extras.putString("descrip",guides.get(position).description);
+                intent.putExtras(extras);
                 startActivity(intent);
             }
         });
+
+
+        //retrieve data from database
+        db.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //on update clear previous data and store new
+                guides.clear();
+                for(DataSnapshot data: snapshot.getChildren()){
+
+                    Guides g=data.getValue(Guides.class);
+                    guides.add(g);
+
+                }
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getApplicationContext(),"Database error",Toast.LENGTH_LONG).show();
+
+            }
+        });
+
         //redirect to tour package
         buttonToPkg.setOnClickListener(new View.OnClickListener() {
             @Override
